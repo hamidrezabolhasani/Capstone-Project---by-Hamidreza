3632ce6c42bdd8174e0b1110619b9d967b33aefb
hamidrezabolhasani <52112125+hamidrezabolhasani@users.noreply.github.com>
2019-06-23 09:54:53 +0430

    Add files via upload

---
+++ Capstone Project - by Hamidreza Bolhasani.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# <h1> Capstone Project </h1>
+
+# It's Hamidreza Bolhasani, PhD. And it's my first assignment of applied data science course by IBM.
+
+# In this assignmet, I've created a user account in Github which is: @hamidrezabolhasani. I followed the instruction to make my first repository with "Github-Example" name and push my first code into it which can be seen in the link below.
+
+# <a href="https://www.dropbox.com/s/jeuc5pe1nou4exf/01-github.PNG?dl=0"> My Github Page </a>
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# <h4><font color="green"> Thanks for your attention!</font></h4>
+# <font color="red"> Hamidreza Bolhasani </font>
